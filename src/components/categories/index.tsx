44a0bbbbe5e3bc6a3d1d0848d8966b7d57c254d1
Mo Amir <mo.amir.code@gmail.com>
2024-03-01 19:15:25 +0530

    categories layout changed

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -4,13 +4,13 @@ import sw from "@/assets/categories/smartwatch.png";
 
 const Categories = () => {
   return (
-    <div className="flex items-center justify-around p-4 shadow-md bg-white">
+    <div className="flex items-center justify-around p-4 max-sm:py-2 max-sm:px-0 shadow-md bg-white">
       {categories.map((item) => (
         <div key={item.name} className="flex flex-col gap-1 items-center justify-center">
-          <div className="w-12 h-12">
+          <div className="w-12 max-sm:w-10 h-12 max-sm:h-10 max-[550px]:w-8 max-[550px]:h-8">
             <Image src={sw} alt="category" />
           </div>
-          <span>{item.name}</span>
+          <span className="max-sm:text-sm max-[550px]:text-[10px]" >{item.name}</span>
         </div>
       ))}
     </div>
