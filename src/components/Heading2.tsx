e6113832b856595942375b4d22f0ce2f0a03b280
Mo Amir <mo.amir.code@gmail.com>
2024-03-01 18:49:07 +0530

    layout fixed

--- src/components/Heading2.tsx
+++ src/components/Heading2.tsx
@@ -2,7 +2,7 @@
 
 const Heading2 = ({text}:{text:string}) => {
   return (
-    <h2 className="text-xl my-4 font-semibold text-secondary-color font-poppins border-b border-secondary-color pb-2" >{text}</h2>
+    <h2 className="text-xl max-[660px]:text-lg max-[460px]:text-sm my-4 font-semibold text-secondary-color font-poppins border-b border-secondary-color pb-2" >{text}</h2>
   )
 }
 
--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -4,8 +4,8 @@ import product from "@/assets/productsImage/dummy2.png";
 const ProductCard = () => {
   return (
     <div>
-      <div className="max-w-[216px] bg-white p-2 rounded-xl smooth_transition hover:shadow-md">
-        <div className="w-[200px] rounded-lg overflow-hidden">
+      <div className="max-w-[216px] max-[460px]:w-[176px] bg-white p-2 rounded-xl smooth_transition hover:shadow-md">
+        <div className="w-[200px] max-[460px]:w-[160px] rounded-lg overflow-hidden">
           <Image src={product} alt="product image" />
         </div>
       </div>
