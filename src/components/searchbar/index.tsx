857199779c41505ac878acc679c576442fdd79e1
Mo Amir <mo.amir.code@gmail.com>
2024-03-03 03:12:33 +0530

    mobile header almost completed

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -1,10 +1,10 @@
 import { TfiSearch } from "react-icons/tfi";
 
-const SearchBar = () => {
+const SearchBar = ({isOnMobile}:{isOnMobile?:boolean}) => {
   return (
-    <div className="flex hover:shadow-md smooth_transition items-center gap-2 justify-start bg-[#F0F5FF] max-sm:max-w-[180px] rounded-md max-sm:py-1 py-2 px-3 flex-grow max-w-[400px] min-w-[200px]" >
+    <div style={isOnMobile? {maxWidth: "100%"} : {}} className={`flex smooth_transition items-center gap-2 justify-start bg-[#F0F5FF] max-sm:max-w-[180px] rounded-md max-sm:py-1 py-2 px-3 flex-grow max-w-[400px] min-w-[200px] ${isOnMobile? "border-[1.5px]" : "hover:shadow-md"}`} >
         <TfiSearch size={20} className="text-secondary-color" />
-        <input type="text" placeholder="Search on Memik" className="outline-none bg-transparent w-full" />
+        <input type="text" placeholder="Search on Memik" className="outline-none text-sm py-1 bg-transparent w-full" />
     </div>
   )
 }
--- src/sections/header/MobileHeader.tsx
+++ src/sections/header/MobileHeader.tsx
@@ -2,11 +2,12 @@ import Image from "next/image"
 import Link from "next/link"
 import logo from "@/assets/brand/logo.png"
 import MobileWallet from "@/components/buttons/MobileWallet"
+import SearchBar from "@/components/searchbar"
 
 
 const MobileHeader = () => {
   return (
-    <header className="py-2 px-4 md:hidden" >
+    <header className="py-2 px-4 md:hidden space-y-4" >
         <div className="flex items-center justify-between gap-1">
             <div className="flex items-center justify-center w-12 h-12 cursor-pointer max-sm:w-10 max-sm:h-10">
               <Link href={"/"}>
@@ -23,6 +24,9 @@ const MobileHeader = () => {
             <MobileWallet amount="23" />
           </div>
 
+          <div>
+            <SearchBar isOnMobile={true} />
+          </div>
     </header>
   )
 }
