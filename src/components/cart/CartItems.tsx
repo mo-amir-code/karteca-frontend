792f8087a19a056cdfa3f0ce18128340ac4383bf
Mo Amir <mo.amir.code@gmail.com>
2024-03-05 16:34:56 +0530

    key added in cartitems

--- src/components/cart/CartItems.tsx
+++ src/components/cart/CartItems.tsx
@@ -5,8 +5,8 @@ const CartItems = () => {
     return (
         <div className="flex-[0.65] w-full mx-2 max-md:mx-0 px-4 rounded-lg shadow-lg py-4 space-y-4" >
             {
-                [1,2].map(()=>(
-                    <CartItem/>
+                [1,2].map((cart, idx)=>(
+                    <CartItem key={idx}/>
                 ))
             }
         </div>
