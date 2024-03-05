9fa5394bfe8354cbf4fa9e9f9406f04a5a515b3c
Mo Amir <mo.amir.code@gmail.com>
2024-03-05 15:59:33 +0530

    mini changes in product details page

--- src/sections/productDetails/Hero.tsx
+++ src/sections/productDetails/Hero.tsx
@@ -6,7 +6,7 @@ import ProductDetails from "@/components/productDetails/ProductDetails";
 const Hero = () => {
 
   return (
-    <div className="flex bg-white max-[900px]:flex-col box-border p-3 gap-4">
+    <div className="flex bg-white max-[900px]:flex-col box-border p-3 max-md:px-0 gap-4">
       <div className="w-[700px] max-[900px]:w-full rounded-lg space-y-2 box-border">
         <Images />
         <ProductActionButton />
@@ -18,7 +18,7 @@ const Hero = () => {
   );
 };
 
-const ProductActionButton = () => {
+export const ProductActionButton = () => {
 
   const handleBuyNow = () => {};
 
--- src/sections/productDetails/ProductSpecifications.tsx
+++ src/sections/productDetails/ProductSpecifications.tsx
@@ -2,14 +2,16 @@ import Specs from "@/HOC/Specs";
 import Highlights from "@/components/productDetails/Highlights";
 import Warranty from "./Warranty";
 import Specifications from "./Specifications";
+import { ProductActionButton } from "./Hero";
 
 const ProductSpecifications = () => {
   return (
-    <div className="flex bg-white p-3">
+    <div className="flex bg-white p-3 max-md:px-0">
       <div className="flex-grow">
         <h2 className="text-lg font-semibold font-lato">Specifications</h2>
         <div className="py-3 space-y-3">
             <SpecsWrapper />
+            <ProductActionButton />
         </div>
       </div>
       <div className="w-[400px] max-[900px]:hidden bg-red-500 h-8"></div>
