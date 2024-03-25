1c128f9f639ceadf53fcf2ae0f0151af3cfb00c5
Mo Amir <mo.amir.code@gmail.com>
2024-03-25 17:02:07 +0530

    types issue

--- src/components/checkout/PaymentArea.tsx
+++ src/components/checkout/PaymentArea.tsx
@@ -5,7 +5,7 @@ const PaymentArea = () => {
   return (
     <div className="flex-[0.35] space-y-4 py-2 w-full max-md:mx-0 mx-2 rounded-lg text-secondary-color">
       {/* <div className="flex item px-3 bg-white">sdsds</div> */}
-      <CartBill />
+      <CartBill totalAmount={0} />
     </div>
   );
 };
