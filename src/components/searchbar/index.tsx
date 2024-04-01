80cb251b03d273b72de3c6f2173924e0f86690a0
Mo Amir <mo.amir.code@gmail.com>
2024-04-01 16:42:05 +0530

    search bar activated

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -1,13 +1,42 @@
-import { memo } from "react";
+"use client";
+import { useRouter } from "next/navigation";
+import { memo, useCallback, useState } from "react";
 import { TfiSearch } from "react-icons/tfi";
 
-const SearchBar = ({isOnMobile}:{isOnMobile?:boolean}) => {
-  return (
-    <div style={isOnMobile? {maxWidth: "100%"} : {}} className={`flex smooth_transition items-center gap-2 justify-start bg-[#F0F5FF] max-sm:max-w-[180px] rounded-md max-sm:py-1 py-2 px-3 flex-grow max-w-[400px] min-w-[200px] ${isOnMobile? "border-[1.5px]" : "hover:shadow-md"}`} >
-        <TfiSearch size={20} className="text-secondary-color" />
-        <input type="text" placeholder="Search on Memik" className="outline-none text-sm py-1 bg-transparent w-full" />
-    </div>
-  )
-}
+const SearchBar = ({ isOnMobile }: { isOnMobile?: boolean }) => {
+  const [query, setQuery] = useState<string | null>(null);
+  const router = useRouter();
 
-export default memo(SearchBar)
+  const handleSearch = useCallback(() => {
+      setQuery(null);  
+      router.push(`/search?query=${query}`);
+  }, [query]);
+
+  return (
+    <div className="flex gap-2">
+      <div
+        style={isOnMobile ? { maxWidth: "100%" } : {}}
+        className={`flex smooth_transition items-center gap-2 justify-start bg-[#F0F5FF] max-sm:max-w-[180px] rounded-md max-sm:py-1 py-2 px-3 flex-grow max-w-[400px] min-w-[200px] ${
+          isOnMobile ? "border-[1.5px]" : "hover:shadow-md"
+        }`}
+      >
+        {!(!!query) && <TfiSearch size={20} className="text-secondary-color" />}
+        <input
+          type="text"
+          value={query || ""}
+          onKeyDown={(event)=>{
+            if(event.key === "Enter" && query) handleSearch()
+          }}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search on Memik"
+          className="outline-none text-sm py-1 bg-transparent w-full"
+        />
+      </div>
+      {!!query && <button onClick={()=>handleSearch()} className="bg-primary-color px-4 rounded-md">
+        <TfiSearch size={20} className="text-white" />
+      </button>}
+    </div>
+  );
+};
+
+export default memo(SearchBar);
