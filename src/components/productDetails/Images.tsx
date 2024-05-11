631884f0ba68247aabdf24149de4b795f2346a0e
Mo Amir <mo.amir.code@gmail.com>
2024-05-11 22:03:12 +0530

    slider added

--- src/components/productDetails/Images.tsx
+++ src/components/productDetails/Images.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import ProductButton from "../buttons/ProductButton";
 import { useRouter } from "next/navigation";
 import { APICartType } from "@/redux/queries/cart/cartTypes";
@@ -21,6 +21,8 @@ import {
   useDeleteUserWishlistItemMutation,
 } from "@/redux/queries/user/userAPI";
 import { useProductContext } from "@/context/ProductContext";
+import Slider, { Settings } from "react-slick";
+import "../heroSlider/slider.css";
 
 const Images = ({
   productThumbnail,
@@ -32,31 +34,39 @@ const Images = ({
   productId: string;
 }) => {
   const [isWishlist, setIsWishlist] = useState<boolean>(false);
-  const [thumbnail, setThumbnail] = useState<string>(productThumbnail);
-  const [images, setImages] = useState<string[]>(productImages);
+  const [images, setImages] = useState<string[]>([
+    productThumbnail,
+    ...productImages,
+  ]);
   const isUserLoggedIn = useAppSelector(selectIsUserLoggedIn);
   const loggedInUserId = useAppSelector(selectLoggedInUserId);
   const userWishlistItems = useAppSelector(selectUserWishlistItems);
   const [addItemToUserWishlist] = useAddUserWishlistItemMutation();
   const [deleteItemFromUserWishlist] = useDeleteUserWishlistItemMutation();
   const router = useRouter();
+  const sliderRef = useRef<Slider>(null);
 
-  const handleChangeMainImage = (idx: number) => {
-    let newThumbnail = thumbnail;
-    let newImages = [...images];
-    setThumbnail(newImages[idx]);
-    newImages[idx] = newThumbnail;
-    setImages(newImages);
+  const handleScrollByImageSelection = (idx: number) => {
+    // let newThumbnail = thumbnail;
+    // let newImages = [...images];
+    // setThumbnail(newImages[idx]);
+    // newImages[idx] = newThumbnail;
+    // setImages(newImages
+    if(sliderRef.current){
+      sliderRef.current.slickGoTo(idx);
+    }
   };
 
   const handleFavourite = async () => {
     try {
       if (isUserLoggedIn) {
-        if (isWishlist){
+        if (isWishlist) {
           setIsWishlist(false);
-          await deleteItemFromUserWishlist({userId: loggedInUserId!, productId});
-        }
-        else{
+          await deleteItemFromUserWishlist({
+            userId: loggedInUserId!,
+            productId,
+          });
+        } else {
           setIsWishlist(true);
           await addItemToUserWishlist({ userId: loggedInUserId!, productId });
         }
@@ -72,36 +82,46 @@ const Images = ({
     }
   }, [userWishlistItems]);
 
+  const settings: Settings = {
+    infinite: true,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <div className="w-full relative flex max-sm:flex-col-reverse gap-2 smooth_transition">
-        <div className="flex-[0.15] overflow-auto flex sm:flex-col items-center justify-between gap-1 overflow-y-auto">
+        <div className="max-sm:w-full max-h-[400px] overflow-auto flex sm:flex-col items-center gap-1">
           {images?.map((image, idx) => (
-            <div
+            <Image
               key={idx}
-              onClick={() => handleChangeMainImage(idx)}
-              className="border-2 border-transparent hover:border-primary-color smooth_transition"
-            >
-              <Image
-                src={image}
-                alt="product"
-                className="cursor-pointer"
-                width={60}
-                height={60}
-              />
-            </div>
+              src={image}
+              alt="product"
+              className="cursor-pointer"
+              width={60}
+              height={60}
+              onClick={()=>handleScrollByImageSelection(idx)}
+            />
           ))}
         </div>
-        <div className="flex-[0.85] flex items-center justify-center">
-          <Image
-            src={thumbnail}
-            alt="product"
-            className="object-cover"
-            width={500}
-            height={500}
-          />
+        <div className="flex items-center justify-center w-full">
+          <div className="w-[340px]">
+            <Slider ref={sliderRef} {...settings}>
+              {images.map((img, idx) => (
+                <Image
+                  key={idx}
+                  src={img}
+                  alt="product"
+                  className="object-cover"
+                  width={500}
+                  height={500}
+                />
+              ))}
+            </Slider>
+          </div>
         </div>
-
         <span
           onClick={() => handleFavourite()}
           className="absolute top-2 right-6"
@@ -127,45 +147,57 @@ export const ProductActionButton = () => {
   const { currentPrice, totalAmount, discount } = useProductContext();
   const [addToCart] = useAddToCartMutation();
 
-  const handleCart = useCallback(async (type: string) => {
-    if (!isUserLoggedIn || !loggedInUserId) {
-      router.push("/auth/signin");
-      return;
-    }
+  const handleCart = useCallback(
+    async (type: string) => {
+      if (!isUserLoggedIn || !loggedInUserId) {
+        router.push("/auth/signin");
+        return;
+      }
 
-    if (!productId || currentPrice === 0 || totalAmount === 0) {
-      toast.error("Something gone wrong!");
-      return;
-    }
+      if (!productId || currentPrice === 0 || totalAmount === 0) {
+        toast.error("Something gone wrong!");
+        return;
+      }
 
-    const color = queries.get("color");
-    const quantity = queries.get("quantity");
+      const color = queries.get("color");
+      const quantity = queries.get("quantity");
 
-    const cartData: APICartType = {
-      userId: loggedInUserId,
-      product: productId,
-      color: color,
-      quantity: quantity,
+      const cartData: APICartType = {
+        userId: loggedInUserId,
+        product: productId,
+        color: color,
+        quantity: quantity,
+        totalAmount,
+        currentPrice,
+        discount,
+      };
+
+      const { data: res, error } = (await addToCart(cartData)) as {
+        data: APIRequestType;
+        error: { data: APIRequestType; status: number };
+      };
+
+      if (res?.success) {
+        toast.success(res.message);
+      }
+
+      if (error && type !== "buy") {
+        toast.error(error.data.message);
+      }
+
+      if (type === "buy") router.push("/user/cart");
+    },
+    [
       totalAmount,
+      queries,
+      addToCart,
+      isUserLoggedIn,
+      loggedInUserId,
       currentPrice,
       discount,
-    };
-
-    const { data: res, error } = (await addToCart(cartData)) as {
-      data: APIRequestType;
-      error: { data: APIRequestType; status: number };
-    };
-
-    if (res?.success) {
-      toast.success(res.message);
-    }
-
-    if (error && type !== "buy") {
-      toast.error(error.data.message);
-    }
-
-    if (type === "buy") router.push("/user/cart");
-  }, [totalAmount, queries, addToCart, isUserLoggedIn, loggedInUserId, currentPrice, discount, productId]);
+      productId,
+    ]
+  );
 
   return (
     <div className="flex items-center bottom_to_top_ani gap-1">
